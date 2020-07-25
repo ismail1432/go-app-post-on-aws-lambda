0b6edf78a7dbf9ad4e52d5a021fc29ab254411ad
Smaine Milianni <smaine.milianni@gmail.com>
2020-07-26 00:48:01 +0100

    remove useless pull and build

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple GO application to call an Symfony API that uses AWS Lambda
+
+### The content of the message will be posted on LinkedIn
---
+++ main.go
@@ -0,0 +1,37 @@
+    package main
+
+    import (
+        "net/http"
+        "html/template"
+    )
+
+    var tpl *template.Template
+
+    func init() {
+        tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+    }
+
+    func main() {
+        http.HandleFunc("/", index)
+        http.ListenAndServe(":8080", nil)
+    }
+
+    func index(w http.ResponseWriter, r *http.Request) {
+        tpl.ExecuteTemplate(w, "index.gohtml", nil)
+    }
+
+    func process_message(w http.ResponseWriter, r *http.Request) {
+        if r.Method != "POST" {
+            http.Redirect(w, r, "/", http.StatusSeeOther)
+            return
+        }
+
+        //fcontent := r.FormValue("linkedin_content")
+
+        values := map[string]string{"username": username, "password": password}
+
+        jsonValue, _ := json.Marshal(values)
+
+        resp, err := http.Post(authAuthenticatorUrl, "application/json", bytes.NewBuffer(jsonValue))
+
+    }
